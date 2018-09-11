1fbe73f6658d8b5df840b792f38ec55639557e7b
JoshT <JoshT@amtimail.com>
2018-09-11 12:38:04 -0400

    pt.2

--- openaiexample.py
+++ openaiexample.py
@@ -11,7 +11,7 @@ LR = 1e-3
 env = gym.make('CartPole-v0')
 goal_steps = 500
 score_requirement = 50
-inital_games = 10000
+initial_games = 10000
 
 def some_random_games_first():
     for episode in range(5):
@@ -23,4 +23,41 @@ def some_random_games_first():
             if done:
                 break
 
-some_random_games_first()
+#some_random_games_first()
+
+def initial_population():
+    env.reset()
+    training_data = []
+    scores = []
+    accepted_scores = []
+    for _ in range(initial_games):
+        score = 0
+        game_memory = []
+        for _ in range(goal_steps):
+            action = random.randrange(0,2)
+            observation, reward, done, info = env.step(action)
+            game_memory.append([observation, action])
+            score += reward
+            if done:
+                break
+        if score >= score_requirement:
+            accepted_scores.append(score)
+            for data in game_memory:
+                if data[1] == 1:
+                    output = [0,1]
+                elif data[1] == 0:
+                    output = [1,0]
+                training_data.append([data[0], output])
+        env.reset()
+        scores.append(score)
+
+    training_data_save = np.array(training_data)
+    np.save('saved.npy', training_data_save)
+
+    print('Average accepted score:', mean(accepted_scores))
+    print('Median accepted score:', mean(accepted_scores))
+    print(Counter(accepted_scores))
+
+    return training_data
+
+initial_population()
