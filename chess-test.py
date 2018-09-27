b265c7f000066715e12bbd70c6e3b905b9176025
JoshT <JoshT@amtimail.com>
2018-09-27 07:42:39 -0400

    I can now draw images easily and I have the piece images saved.
    I might consider changing the background again, the game looks wonky...

--- chess-test.py
+++ chess-test.py
@@ -5,9 +5,19 @@ import pygame
 prog_name = "AI Chess Trainer"
 version = "ALPHA 0.0.1"
 
-square_start_pixel = [35, 35]
-square_end_pixel = [465, 465]
-square_size = [54, 54]
+screen_size = [650, 645]
+
+square_start_pixel = [50, 50]
+square_end_pixel = [600, 600]
+square_size = 69 #square
+square_centers = [[[square_start_pixel[0]+square_size/2, square_start_pixel[1]+square_size/2], [square_start_pixel[0]+square_size+square_size/2, square_start_pixel[1]+square_size/2], [square_start_pixel[0]+2*square_size+square_size/2, square_start_pixel[1]+square_size/2], [square_start_pixel[0]+3*square_size+square_size/2, square_start_pixel[1]+square_size/2], [square_start_pixel[0]+4*square_size+square_size/2, square_start_pixel[1]+square_size/2], [square_start_pixel[0]+5*square_size+square_size/2, square_start_pixel[1]+square_size/2], [square_start_pixel[0]+6*square_size+square_size/2, square_start_pixel[1]+square_size/2], [square_start_pixel[0]+7*square_size+square_size/2, square_start_pixel[1]+square_size/2]],
+                  [[square_start_pixel[0]+square_size/2, square_start_pixel[1]+square_size+square_size/2], [square_start_pixel[0]+square_size+square_size/2, square_start_pixel[1]+square_size+square_size/2], [square_start_pixel[0]+2*square_size+square_size/2, square_start_pixel[1]+square_size+square_size/2], [square_start_pixel[0]+3*square_size+square_size/2, square_start_pixel[1]+square_size+square_size/2], [square_start_pixel[0]+4*square_size+square_size/2, square_start_pixel[1]+square_size+square_size/2], [square_start_pixel[0]+5*square_size+square_size/2, square_start_pixel[1]+square_size+square_size/2], [square_start_pixel[0]+6*square_size+square_size/2, square_start_pixel[1]+square_size+square_size/2], [square_start_pixel[0]+7*square_size+square_size/2, square_start_pixel[1]+square_size+square_size/2]],
+                  [[square_start_pixel[0]+square_size/2, square_start_pixel[1]+2*square_size+square_size/2], [square_start_pixel[0]+square_size+square_size/2, square_start_pixel[1]+2*square_size+square_size/2], [square_start_pixel[0]+2*square_size+square_size/2, square_start_pixel[1]+2*square_size+square_size/2], [square_start_pixel[0]+3*square_size+square_size/2, square_start_pixel[1]+2*square_size+square_size/2], [square_start_pixel[0]+4*square_size+square_size/2, square_start_pixel[1]+2*square_size+square_size/2], [square_start_pixel[0]+5*square_size+square_size/2, square_start_pixel[1]+2*square_size+square_size/2], [square_start_pixel[0]+6*square_size+square_size/2, square_start_pixel[1]+2*square_size+square_size/2], [square_start_pixel[0]+7*square_size+square_size/2, square_start_pixel[1]+2*square_size+square_size/2]],
+                  [[square_start_pixel[0]+square_size/2, square_start_pixel[1]+3*square_size+square_size/2], [square_start_pixel[0]+square_size+square_size/2, square_start_pixel[1]+3*square_size+square_size/2], [square_start_pixel[0]+2*square_size+square_size/2, square_start_pixel[1]+3*square_size+square_size/2], [square_start_pixel[0]+3*square_size+square_size/2, square_start_pixel[1]+3*square_size+square_size/2], [square_start_pixel[0]+4*square_size+square_size/2, square_start_pixel[1]+3*square_size+square_size/2], [square_start_pixel[0]+5*square_size+square_size/2, square_start_pixel[1]+3*square_size+square_size/2], [square_start_pixel[0]+6*square_size+square_size/2, square_start_pixel[1]+3*square_size+square_size/2], [square_start_pixel[0]+7*square_size+square_size/2, square_start_pixel[1]+3*square_size+square_size/2]],
+                  [[square_start_pixel[0]+square_size/2, square_start_pixel[1]+4*square_size+square_size/2], [square_start_pixel[0]+square_size+square_size/2, square_start_pixel[1]+4*square_size+square_size/2], [square_start_pixel[0]+2*square_size+square_size/2, square_start_pixel[1]+4*square_size+square_size/2], [square_start_pixel[0]+3*square_size+square_size/2, square_start_pixel[1]+4*square_size+square_size/2], [square_start_pixel[0]+4*square_size+square_size/2, square_start_pixel[1]+4*square_size+square_size/2], [square_start_pixel[0]+5*square_size+square_size/2, square_start_pixel[1]+4*square_size+square_size/2], [square_start_pixel[0]+6*square_size+square_size/2, square_start_pixel[1]+4*square_size+square_size/2], [square_start_pixel[0]+7*square_size+square_size/2, square_start_pixel[1]+4*square_size+square_size/2]],
+                  [[square_start_pixel[0]+square_size/2, square_start_pixel[1]+5*square_size+square_size/2], [square_start_pixel[0]+square_size+square_size/2, square_start_pixel[1]+5*square_size+square_size/2], [square_start_pixel[0]+2*square_size+square_size/2, square_start_pixel[1]+5*square_size+square_size/2], [square_start_pixel[0]+3*square_size+square_size/2, square_start_pixel[1]+5*square_size+square_size/2], [square_start_pixel[0]+4*square_size+square_size/2, square_start_pixel[1]+5*square_size+square_size/2], [square_start_pixel[0]+5*square_size+square_size/2, square_start_pixel[1]+5*square_size+square_size/2], [square_start_pixel[0]+6*square_size+square_size/2, square_start_pixel[1]+5*square_size+square_size/2], [square_start_pixel[0]+7*square_size+square_size/2, square_start_pixel[1]+5*square_size+square_size/2]],
+                  [[square_start_pixel[0]+square_size/2, square_start_pixel[1]+6*square_size+square_size/2], [square_start_pixel[0]+square_size+square_size/2, square_start_pixel[1]+6*square_size+square_size/2], [square_start_pixel[0]+2*square_size+square_size/2, square_start_pixel[1]+6*square_size+square_size/2], [square_start_pixel[0]+3*square_size+square_size/2, square_start_pixel[1]+6*square_size+square_size/2], [square_start_pixel[0]+4*square_size+square_size/2, square_start_pixel[1]+6*square_size+square_size/2], [square_start_pixel[0]+5*square_size+square_size/2, square_start_pixel[1]+6*square_size+square_size/2], [square_start_pixel[0]+6*square_size+square_size/2, square_start_pixel[1]+6*square_size+square_size/2], [square_start_pixel[0]+7*square_size+square_size/2, square_start_pixel[1]+6*square_size+square_size/2]],
+                  [[square_start_pixel[0]+square_size/2, square_start_pixel[1]+7*square_size+square_size/2], [square_start_pixel[0]+square_size+square_size/2, square_start_pixel[1]+7*square_size+square_size/2], [square_start_pixel[0]+2*square_size+square_size/2, square_start_pixel[1]+7*square_size+square_size/2], [square_start_pixel[0]+3*square_size+square_size/2, square_start_pixel[1]+7*square_size+square_size/2], [square_start_pixel[0]+4*square_size+square_size/2, square_start_pixel[1]+7*square_size+square_size/2], [square_start_pixel[0]+5*square_size+square_size/2, square_start_pixel[1]+7*square_size+square_size/2], [square_start_pixel[0]+6*square_size+square_size/2, square_start_pixel[1]+7*square_size+square_size/2], [square_start_pixel[0]+7*square_size+square_size/2, square_start_pixel[1]+7*square_size+square_size/2]]]
 
 # board = chess.Board()
 
@@ -33,19 +43,24 @@ def main():
     logo = pygame.image.load("chess_title_icon.png")                #Load a logo
     pygame.display.set_icon(logo)                                   #Set icon
     pygame.display.set_caption(prog_name + "   -   " + version)     #Set program text
-    screen = pygame.display.set_mode((500,500))                     #Set screensize
-    bg = pygame.image.load("chess_background.jpg")                  #Load board background
-    screen.blit(bg, (0,0))
-    pygame.display.flip()
+    screen = pygame.display.set_mode(screen_size)                     #Set screensize
+    drawBoard(screen)
     running = True                                                  #Define the game as running
     while running:
         for event in pygame.event.get():                            #loop throughall the events
             if event.type == pygame.QUIT:
                 running = False                                     #Stop running if the QUIT event is received
-            if event.type == pygame.MOUSEBUTTONDOWN:
-                clickedPos = pygame.mouse.get_pos()
-                clickedSquare = getClickedSquare(clickedPos)
-                print(clickedSquare)
+
+def drawBoard(screen):#board):
+    drawImage(screen, "chess_background_w.jpg", 0, 0)
+    for row in square_centers:
+        for col in row:
+            drawImage(screen, "chess_pieces\Chess_plt60.png", col[0]-30, col[1]-30) #draw a white pawn everywhere
+
+def drawImage(screen, filename, xpos, ypos):
+    img = pygame.image.load(filename)                  #Load board background
+    screen.blit(img, (xpos,ypos))
+    pygame.display.flip()
 
 def getClickedPiece(clickedPos):
     print(clickedPos)
@@ -73,21 +88,21 @@ def getClickedSquare(clickedPos):#, bool isWhite):
         col = "h"
     #Define row
     if square_start_pixel[1] < clickedPos[1] < (square_start_pixel[1] + 1 * square_size[1]):
-        row = "1"
-    elif square_start_pixel[1] < clickedPos[1] < (square_start_pixel[1] + 2 * square_size[1]):
-        row = "2"
-    elif square_start_pixel[1] < clickedPos[1] < (square_start_pixel[1] + 3 * square_size[1]):
-        row = "3"
-    elif square_start_pixel[1] < clickedPos[1] < (square_start_pixel[1] + 4 * square_size[1]):
-        row = "4"
-    elif square_start_pixel[1] < clickedPos[1] < (square_start_pixel[1] + 5 * square_size[1]):
-        row = "5"
-    elif square_start_pixel[1] < clickedPos[1] < (square_start_pixel[1] + 6 * square_size[1]):
-        row = "6"
-    elif square_start_pixel[1] < clickedPos[1] < (square_start_pixel[1] + 7 * square_size[1]):
-        row = "7"
-    elif square_start_pixel[1] < clickedPos[1] < (square_start_pixel[1] + 8 * square_size[1]):
         row = "8"
+    elif square_start_pixel[1] < clickedPos[1] < (square_start_pixel[1] + 2 * square_size[1]):
+        row = "7"
+    elif square_start_pixel[1] < clickedPos[1] < (square_start_pixel[1] + 3 * square_size[1]):
+        row = "6"
+    elif square_start_pixel[1] < clickedPos[1] < (square_start_pixel[1] + 4 * square_size[1]):
+        row = "5"
+    elif square_start_pixel[1] < clickedPos[1] < (square_start_pixel[1] + 5 * square_size[1]):
+        row = "4"
+    elif square_start_pixel[1] < clickedPos[1] < (square_start_pixel[1] + 6 * square_size[1]):
+        row = "3"
+    elif square_start_pixel[1] < clickedPos[1] < (square_start_pixel[1] + 7 * square_size[1]):
+        row = "2"
+    elif square_start_pixel[1] < clickedPos[1] < (square_start_pixel[1] + 8 * square_size[1]):
+        row = "1"
 
     if col == "":
         row = ""
